af871f3b5d403a28c47880643ad6ab8a5a43fbc0
proshantosaha <proshantoll71@gmail.com>
2024-12-30 23:05:14 +0600

    added client id and client secret

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 export const {
   handlers: { get, post },
