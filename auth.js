8278ab684f38ea4d68d68c05e87b60098f2de6ad
proshantosaha <proshantoll71@gmail.com>
2025-01-01 16:28:37 +0600

    google and facebook authentication completed

--- auth.js
+++ auth.js
@@ -1,16 +1,22 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import FacebookProvider from "next-auth/providers/facebook";
+
 export const {
-  handlers: { get, post },
+  handlers: { GET, POST },
   auth,
   signIn,
   signOut,
 } = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "process.env.GOOGLE_CLIENT_ID",
-      clientSecret: "process.env.GOOGLE_CLIENT_SECRET",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
 });
