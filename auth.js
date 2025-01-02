d24bc7612020f653a95a864c479e056318405046
proshantosaha <proshantoll71@gmail.com>
2025-01-02 15:30:26 +0600

    credential log in

--- auth.js
+++ auth.js
@@ -4,6 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import mongoClientPromise from "./database/mongoClientPromise";
+import { userModel } from "./models/user-model";
+import Credentials from "next-auth/providers/credentials";
 
 export const {
   handlers: { GET, POST },
@@ -14,7 +16,36 @@ export const {
   adapter: MongoDBAdapter(mongoClientPromise, {
     databaseName: process.env.ENVIRONMENT,
   }),
+  session: {
+    strategy: "jwt",
+  },
   providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        // const response = await fetch(request)
+        if (credentials === null) return null;
+        // return (await response.json()) ?? null
+        try {
+          const user = await userModel.findOne({ email: credentials.email });
+          if (user) {
+            const isMatch = user.email === credentials.email;
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("email and password not match");
+            }
+          } else {
+            throw new Error("user not found");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
